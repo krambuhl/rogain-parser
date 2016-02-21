d94cf70e8f5ea0551dd17d135d9be7cb60db572a
Evan Krambuhl <thekrambuhl@gmail.com>
2016-02-20 23:27:39 -0800

    singletexttree tests

--- test/isSingleTextTree.test.js
+++ test/isSingleTextTree.test.js
@@ -1,15 +1,72 @@
 var test = require('tape');
-var through = require('through2');
-
-var tutils = require('rogain-tree-utils');
 var isSingleTextTree = require('../dist/isSingleTextTree');
 
-var list = [
-  { data: "Read More", type: "text" }
+
+var elist = [];
+
+test('isSingleTextTree(treeList) :: empty list', function(t) {
+  t.plan(1);
+  t.equal(isSingleTextTree(elist), false);
+});
+
+
+var slist = [
+  { type: "text", data: "Read More" }
 ];
 
-test('isSingleTextTree(treeList)', function(t) {
+test('isSingleTextTree(treeList) :: single text list', function(t) {
   t.plan(1);
+  t.equal(isSingleTextTree(slist), true);
+});
 
-  // var res = isSingleTextTree()
+
+var mlist = [
+  { type: "text", data: "Item 1" },
+  { type: "text", data: "Item 2" },
+  { type: "text", data: "Item 3" },
+];
+
+test('isSingleTextTree(treeList) :: multiple text list', function(t) {
+  t.plan(1);
+  t.equal(isSingleTextTree(mlist), false);
+});
+
+
+var mxlist = [
+  { type: "helper", name: "Helper" },
+  { type: "text", data: "Read More" }
+];
+
+test('isSingleTextTree(treeList) :: multiple mixed list', function(t) {
+  t.plan(1);
+  t.equal(isSingleTextTree(mxlist), false);
+});
+
+
+var molist = [
+  { type: "helper", name: "Helper" },
+  { type: "component", name: "Heading" }
+];
+
+test('isSingleTextTree(treeList) :: multiple other list', function(t) {
+  t.plan(1);
+  t.equal(isSingleTextTree(molist), false);
+});
+
+
+var nslist = [
+  { type: "helper", name: "Helper" }
+];
+
+test('isSingleTextTree(treeList) :: single other list', function(t) {
+  t.plan(1);
+  t.equal(isSingleTextTree(nslist), false);
+});
+
+
+var wlist = [{ }];
+
+test('isSingleTextTree(treeList) :: weird list', function(t) {
+  t.plan(1);
+  t.equal(isSingleTextTree(wlist), false);
 });
