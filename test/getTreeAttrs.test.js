7ac68fba5572cae2a69d7e76331dc666a33ce621
Evan Krambuhl <thekrambuhl@gmail.com>
2016-02-21 01:16:54 -0800

    get tree attrs test

--- test/getTreeAttrs.test.js
+++ test/getTreeAttrs.test.js
@@ -1,8 +1,51 @@
 var test = require('tape');
-var isSingleTextTree = require('../dist/isSingleTextTree');
+var getTreeAttrs = require('../dist/getTreeAttrs');
 
-test('getTreeType(tree) :: tag', function(t) {
-  t.plan(1);
-  var res = getTreeType({ type: "tag", name: "a" }, helpers);
-  t.equal(res, 'tag');
+test('getTreeAttrs(attrs) :: no attributes', function(t) {
+  t.plan(2);
+  t.equal(getTreeAttrs() === undefined, true);
+  t.equal(getTreeAttrs({}) === undefined, true);
+});
+
+test('getTreeAttrs(attrs) :: simple', function(t) {
+  t.plan(3);
+
+  var res = getTreeAttrs({ 'no-children': 'no-children' });
+  
+  t.equal(res[0].type, 'attr');
+  t.equal(res[0].name, 'no-children');
+  t.equal(res[0].data, 'no-children');
+});
+
+test('getTreeAttrs(attrs) :: multiple', function(t) {
+  t.plan(7);
+  
+  var res = getTreeAttrs({ 
+    hen: '{hen}',
+    roost: 'mountain'
+  });
+
+  t.equal(res[0].type, 'attr');
+  t.equal(res[0].name, 'hen');
+  t.equal(res[0].data[0].type, 'variable');
+  t.equal(res[0].data[0].path, 'hen');
+
+  t.equal(res[1].type, 'attr');
+  t.equal(res[1].name, 'roost');
+  t.equal(res[1].data, 'mountain');
+});
+
+test('getTreeAttrs(attrs) :: complex', function(t) {
+  t.plan(8);
+
+  var res = getTreeAttrs({ '{data}': 'bungle {colors|first}' });
+  
+  t.equal(res[0].type, 'attr');
+  t.equal(res[0].name[0].type, 'variable');
+  t.equal(res[0].name[0].path, 'data');
+  t.equal(res[0].data[0].type, 'text');
+  t.equal(res[0].data[0].data, 'bungle ');
+  t.equal(res[0].data[1].type, 'variable');
+  t.equal(res[0].data[1].path, 'colors');
+  t.equal(res[0].data[1].filters.length, 1);
 });
